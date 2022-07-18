c6f556eb6638d938069280e1714c3d1a812fd921
Dovellous <dovelous@gmail.com>
2022-07-18 15:17:52 +0200

    added android platform

--- src/system/libs/dovellous-f7-mobile/src/libraries/storage/json-service.ts
+++ src/system/libs/dovellous-f7-mobile/src/libraries/storage/json-service.ts
@@ -19,8 +19,14 @@ const JSONService = {
 
     seperator: "/",
 
-    init: (filename: any, autosave:boolean = true, pretty:boolean = false, seperator: any = "/") => {
+    platform: "web",
 
+    usePromise: false,
+
+    init: (filename: any, autosave:boolean = true, pretty:boolean = false, seperator: any = "/", platform: any = "web", usePromise: boolean = false) => {
+
+        JSONService.platform = platform;
+        JSONService.usePromise = usePromise;
         JSONService.autosave = autosave;
         JSONService.pretty = pretty;
         JSONService.seperator = seperator;
@@ -28,7 +34,35 @@ const JSONService = {
         JSONService.loadData(null, null);
 
     },
+    load: (callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.loadDataWeb(callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.loadData(callbackFunctionSuccess, callbackFunctionError);
+    },
     loadData: async (callbackFunctionSuccess, callbackFunctionError) => {
+        try{
+            const fileObject = {
+                path: JSONService.path,
+                directory: Directory.Documents,
+                encoding: Encoding.UTF8,
+            };
+            const _data = await Filesystem.readFile(fileObject);
+            JSONService.data = JSON.parse(_data);
+            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
+                callbackFunctionSuccess(JSONService.data);
+            }else{
+                return JSONService.data;
+            }
+        }catch(error){
+            if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
+                callbackFunctionError(error);
+            }else{
+                return;
+            }
+        }
+    },
+    loadDataWeb: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
             const fileObject = {
                 path: JSONService.path,
@@ -66,6 +100,12 @@ const JSONService = {
             }
         });
     }, 
+    get: (path, callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.getDataWeb(path, callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.getData(path, callbackFunctionSuccess, callbackFunctionError);
+    },
     getData: async (path, callbackFunctionSuccess, callbackFunctionError) => {
         try{
             let pathKeys = path.split(JSONService.seperator);
@@ -82,12 +122,29 @@ const JSONService = {
                 return;
             }
         }
+    }, 
+    add: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.addDataWeb(path, data, callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.addData(path, data, callbackFunctionSuccess, callbackFunctionError);
     },    
-    addData: ( key: any) => {
+    addData: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
 
 
 
-    },
+    },   
+    addDataWeb: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
+
+
+
+    }, 
+    save: (callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.saveDataWeb(callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.saveData(callbackFunctionSuccess, callbackFunctionError);
+    }, 
     saveData: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
             const fileObject = {
@@ -105,7 +162,31 @@ const JSONService = {
                 callbackFunctionError(error);
             }
         }
-    },
+    }, 
+    saveDataWeb: async (callbackFunctionSuccess, callbackFunctionError) => {
+        try{
+            const fileObject = {
+                path: JSONService.path,
+                data: JSON.stringify(JSONService.data, null, JSONService.pretty),
+                directory: Directory.Documents,
+                encoding: Encoding.UTF8,
+            };
+            await Filesystem.writeFile(fileObject);
+            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
+                callbackFunctionSuccess(fileObject);
+            }
+        }catch(error){
+            if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
+                callbackFunctionError(error);
+            }
+        }
+    }, 
+    del: (callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.deleteDataWeb(callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.deleteData(callbackFunctionSuccess, callbackFunctionError);
+    }, 
     deleteData: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
             const fileObject = {
@@ -121,16 +202,51 @@ const JSONService = {
                 callbackFunctionError(error);
             }
         }
+    }, 
+    deleteDataWeb: async (callbackFunctionSuccess, callbackFunctionError) => {
+        try{
+            const fileObject = {
+                path: JSONService.path,
+                directory: Directory.Documents,
+            };
+            await Filesystem.deleteFile(fileObject);
+            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
+                callbackFunctionSuccess(fileObject);
+            }
+        }catch(error){
+            if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
+                callbackFunctionError(error);
+            }
+        }
     },
-    deleteKey: ( key: any) => {
+    delKey: (key, callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.deleteKey(key, callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.deleteKeyWeb(key, callbackFunctionSuccess, callbackFunctionError);
+    }, 
+    deleteKey: (key, callbackFunctionSuccess, callbackFunctionError) => {
 
 
 
     },
+    deleteKeyWeb: (key, callbackFunctionSuccess, callbackFunctionError) => {
+
+
+
+    },
+    reload: (key, callbackFunctionSuccess, callbackFunctionError) => {
+        if(JSONService.platform==="web"){
+            return JSONService.reloadData(callbackFunctionSuccess, callbackFunctionError);
+        }
+        return JSONService.reloadDataWeb(callbackFunctionSuccess, callbackFunctionError);
+    }, 
     reloadData: () => {
         JSONService.loadData(null, null);
     },
-    
+    reloadDataWeb: () => {
+        JSONService.loadData(null, null);
+    },
 }
 
 export default JSONService;
