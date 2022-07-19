55c24ab31de91690fc27ac54110668ddf38e6e1a
Dovellous <dovelous@gmail.com>
2022-07-19 15:04:23 +0200

    added filemanager
    -filemanager
    -optimized json db manager

--- src/system/libs/dovellous-f7-mobile/src/libraries/storage/json-service.ts
+++ src/system/libs/dovellous-f7-mobile/src/libraries/storage/json-service.ts
@@ -1,5 +1,6 @@
 import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
-
+import CapacitorStorage from './capacitor-storage';
+ 
 // The first argument is the database filename. If no extension, '.json' is assumed and automatically added.
 // The second argument is used to tell the DB to save after each push
 // If you put false, you'll have to call the save() method.
@@ -15,7 +16,7 @@ const JSONService = {
 
     autosave: true,
 
-    pretty: false,
+    formatted: false,
 
     seperator: "/",
 
@@ -23,12 +24,12 @@ const JSONService = {
 
     usePromise: false,
 
-    init: (filename: any, autosave:boolean = true, pretty:boolean = false, seperator: any = "/", platform: any = "web", usePromise: boolean = false) => {
+    init: (filename: any, autosave:boolean = true, formatted:boolean = false, seperator: any = "/", platform: any = "web", usePromise: boolean = false) => {
 
         JSONService.platform = platform;
         JSONService.usePromise = usePromise;
         JSONService.autosave = autosave;
-        JSONService.pretty = pretty;
+        JSONService.formatted = formatted;
         JSONService.seperator = seperator;
         JSONService.path =  `/io.dovellous.app/state/data/${filename}`;
         JSONService.loadData(null, null);
@@ -42,18 +43,22 @@ const JSONService = {
     },
     loadData: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
-            const fileObject = {
-                path: JSONService.path,
-                directory: Directory.Documents,
-                encoding: Encoding.UTF8,
-            };
-            const _data = await Filesystem.readFile(fileObject);
-            JSONService.data = JSON.parse(_data);
-            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
-                callbackFunctionSuccess(JSONService.data);
-            }else{
-                return JSONService.data;
-            }
+            
+            CapacitorStorage.getKey(JSONService.path, (data)=>{
+                JSONService.data = data;
+                if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
+                    callbackFunctionSuccess(data);
+                }else{
+                    return;
+                }
+            }, (error)=>{
+                if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
+                    callbackFunctionError(error);
+                }else{
+                    return;
+                }
+            });
+
         }catch(error){
             if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
                 callbackFunctionError(error);
@@ -63,19 +68,24 @@ const JSONService = {
         }
     },
     loadDataWeb: async (callbackFunctionSuccess, callbackFunctionError) => {
+        
         try{
-            const fileObject = {
-                path: JSONService.path,
-                directory: Directory.Documents,
-                encoding: Encoding.UTF8,
-            };
-            const _data = await Filesystem.readFile(fileObject);
-            JSONService.data = JSON.parse(_data);
-            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
-                callbackFunctionSuccess(JSONService.data);
-            }else{
-                return JSONService.data;
-            }
+            
+            CapacitorStorage.getKey(JSONService.path, (data)=>{
+                JSONService.data = data;
+                if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
+                    callbackFunctionSuccess(data);
+                }else{
+                    return;
+                }
+            }, (error)=>{
+                if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
+                    callbackFunctionError(error);
+                }else{
+                    return;
+                }
+            });
+
         }catch(error){
             if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
                 callbackFunctionError(error);
@@ -83,28 +93,6 @@ const JSONService = {
                 return;
             }
         }
-    }, 
-    loadDataPromise: async () => {
-        return new Promise(function(resolve, reject) {
-            try{
-                const fileObject = {
-                    path: JSONService.path,
-                    directory: Directory.Documents,
-                    encoding: Encoding.UTF8,
-                };
-                const _data = await Filesystem.readFile(fileObject);
-                JSONService.data = JSON.parse(_data);
-                resolve(JSONService.data);
-            }catch(error){
-                reject(error);
-            }
-        });
-    }, 
-    get: (path, callbackFunctionSuccess, callbackFunctionError) => {
-        if(JSONService.platform==="web"){
-            return JSONService.getDataWeb(path, callbackFunctionSuccess, callbackFunctionError);
-        }
-        return JSONService.getData(path, callbackFunctionSuccess, callbackFunctionError);
     },
     getData: async (path, callbackFunctionSuccess, callbackFunctionError) => {
         try{
@@ -122,52 +110,86 @@ const JSONService = {
                 return;
             }
         }
-    }, 
-    add: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
-        if(JSONService.platform==="web"){
-            return JSONService.addDataWeb(path, data, callbackFunctionSuccess, callbackFunctionError);
-        }
-        return JSONService.addData(path, data, callbackFunctionSuccess, callbackFunctionError);
-    },    
+    },  
     addData: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
-
-
-
-    },   
-    addDataWeb: (path, data, callbackFunctionSuccess, callbackFunctionError) => {
-
-
-
-    }, 
-    save: (callbackFunctionSuccess, callbackFunctionError) => {
-        if(JSONService.platform==="web"){
-            return JSONService.saveDataWeb(callbackFunctionSuccess, callbackFunctionError);
-        }
-        return JSONService.saveData(callbackFunctionSuccess, callbackFunctionError);
-    }, 
-    saveData: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
-            const fileObject = {
-                path: JSONService.path,
-                data: JSON.stringify(JSONService.data, null, JSONService.pretty),
-                directory: Directory.Documents,
-                encoding: Encoding.UTF8,
-            };
-            await Filesystem.writeFile(fileObject);
-            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){
-                callbackFunctionSuccess(fileObject);
+            JSONService.updateJsonObject(path, data, JSONService.data);
+            JSONService.autosave?JSONService.saveData(null, null);
+            if((typeof callbackFunctionSuccess).toString().toLowerCase() === "function"){                
+                callbackFunctionSuccess(JSONService.data);
+            }else{
+                return JSONService.data;
             }
         }catch(error){
             if((typeof callbackFunctionError).toString().toLowerCase() === "function"){
                 callbackFunctionError(error);
+            }else{
+                return;
             }
         }
-    }, 
-    saveDataWeb: async (callbackFunctionSuccess, callbackFunctionError) => {
+    },  
+    addDataAfterId: (afterId, data, callbackFunctionSuccess, callbackFunctionError) => {
+
+        JSONService.data = JSONService.addNewObject(JSONService.data, afterId, data);
+
+    },
+    addNewObject: (mainContainer, idTobeMatched, newObject) => {
+        for (let i = 0; i < mainContainer.length; i++) {
+          if (mainContainer[i].id == idTobeMatched) {
+            let indexOfNewObject = i + 1;
+            mainContainer.splice(indexOfNewObject, 0, newObject);
+            return mainContainer;
+          } else if (mainContainer[i].data && mainContainer[i].data.length) {
+            mainContainer[i].data = JSONService.addNewObject(mainContainer[i].data, idTobeMatched, newObject);
+          }
+        }
+        return mainContainer;
+    },
+    constructObject: (path, data) => {
+        const res = {};
+            let ref = res;
+            let pathArr = path.split(JSONService.seperator.toString());
+            let counter=0;
+            while(counter<pathArr.length){
+            let segmentData = ref[pathArr[counter]];
+               ref[pathArr[counter]] = segmentData?segmentData:{};
+               counter === (pathArr.length - 1)?ref[pathArr[counter]] = data:null;
+               ref = ref[pathArr[counter]];
+               counter++;
+            };
+            return res;
+    },
+    updateJsonObject: (path, value, obj) => {
+        var objValue = value;
+        try {
+            objValue = JSON.parse(value);
+        } catch (e) { } //eat the error, must not be json so carry on... Hack to do a valid JSON check
+    
+        var parts = path.split("."), part;
+        var last = parts.pop();
+        while (part = parts.shift()) {
+            if (typeof obj[part] != "object")
+                obj[part] = {};
+            obj = obj[part];
+        }
+        if (obj.hasOwnProperty(last) && obj[last] && obj[last].constructor === Array) {
+            obj[last].push(objValue);
+        }
+        else if (obj.hasOwnProperty(last) && obj[last]) {
+            var objArray = [];
+            objArray.push(obj[last])
+            objArray.push(objValue);
+            obj[last] = objArray;
+        }
+        else {
+            obj[last] = objValue;
+        }
+    },
+    saveData: async (callbackFunctionSuccess, callbackFunctionError) => {
         try{
             const fileObject = {
                 path: JSONService.path,
-                data: JSON.stringify(JSONService.data, null, JSONService.pretty),
+                data: JSON.stringify(JSONService.data, undefined, JSONService.formatted ?  2 : "\t"),
                 directory: Directory.Documents,
                 encoding: Encoding.UTF8,
             };
@@ -235,17 +257,17 @@ const JSONService = {
 
 
     },
-    reload: (key, callbackFunctionSuccess, callbackFunctionError) => {
+    reload: (callbackFunctionSuccess, callbackFunctionError) => {
         if(JSONService.platform==="web"){
             return JSONService.reloadData(callbackFunctionSuccess, callbackFunctionError);
         }
         return JSONService.reloadDataWeb(callbackFunctionSuccess, callbackFunctionError);
     }, 
-    reloadData: () => {
-        JSONService.loadData(null, null);
+    reloadData: (callbackFunctionSuccess, callbackFunctionError) => {
+        JSONService.loadData(callbackFunctionSuccess, callbackFunctionError);
     },
-    reloadDataWeb: () => {
-        JSONService.loadData(null, null);
+    reloadDataWeb: (callbackFunctionSuccess, callbackFunctionError) => {
+        JSONService.loadData(callbackFunctionSuccess, callbackFunctionError);
     },
 }
 
